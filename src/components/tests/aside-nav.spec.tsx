960cfdf560f4d6d306f24f7801395ce1e5e42ac1
Fellipe Neves <fellipe2.0e@gmail.com>
2023-12-09 20:49:44 -0300

    chore: add explanation what comes from jest and testing library

--- src/components/tests/aside-nav.spec.tsx
+++ src/components/tests/aside-nav.spec.tsx
@@ -6,12 +6,12 @@ import router from 'react-router';
 import { AsideNav } from '../aside-nav';
 import { QueryClientProvider, QueryClient } from 'react-query';
 
-const mockNavigate = jest.fn();
+const mockNavigate = jest.fn(); //Comes from jest - Simulate a function
 
 jest.mock('react-router', () => ({
-    ...jest.requireActual('react-router'),
-    useNavigate: () => mockNavigate
-}))
+    ...jest.requireActual('react-router'), //Require the actual module
+    useNavigate: () => mockNavigate //Change the original execution to a mock
+})) //Comes from jest - Simulates the behavior from import 'react-router'
 
 const renderAsideComponent = () => {
     const queryClient = new QueryClient();
@@ -22,42 +22,42 @@ const renderAsideComponent = () => {
             <AsideNav />
         </QueryClientProvider>
         </BrowserRouter>
-    );
+    ); //Comes from jest - Render a element, creating in Virtual DOM 
 
     return { queryClient }
 }
 
-describe("AsideNav", () => {
-    it("should render correctly", () => {
+describe("AsideNav", () => { //Comes from jest - Description from all tests from this file
+    it("should render correctly", () => { //Comes from jest - Describe especif test
         renderAsideComponent();
 
-        expect(screen.getByText("All files")).toBeInTheDocument();
-        expect(screen.getByText("Favorites")).toBeInTheDocument();
-        expect(screen.getByText("New")).toBeInTheDocument();
+        expect(screen.getByText("All files")).toBeInTheDocument(); //Expect Comes from jest - Expected behavior  
+        expect(screen.getByText("Favorites")).toBeInTheDocument(); //screen object (element created on render method), toBeInTheDocument (checks if element searched by getByText) it's in the documment
+        expect(screen.getByText("New")).toBeInTheDocument(); //comes from testing library
     });
 
     //TODO: ABORDAR REFATORAÇÃO [x]
-    // TODO: explicar o que vem do jest e o que vem da testing library []
+    // TODO: explicar o que vem do jest e o que vem da testing library [x]
     // TODO: explicar o for each []
     it("should call navigate with correct params", () => {
         renderAsideComponent();
 
-        const btnAllFiles = screen.getByText("All files")
+        const btnAllFiles = screen.getByText("All files") //Comes from testing library - Search a element in rendered component before
 
-        fireEvent.click(btnAllFiles);
+        fireEvent.click(btnAllFiles); //Comes from testing library - executes a event, on this case, execute click action on searched element
 
-        expect(mockNavigate).toHaveBeenCalled();
-        expect(mockNavigate).toHaveBeenCalledWith("/");
+        expect(mockNavigate).toHaveBeenCalled(); //Expect Comes from jest - Expected navigation function to be called
+        expect(mockNavigate).toHaveBeenCalledWith("/");// and expected function to be called passing "/" as parameter
     })
 
     it.each([["All files", "/"], ["Favorites", "/favorites"]])("testings %s", (text, path) => {
         renderAsideComponent();
 
-        const btn = screen.getByText(text);
+        const btn = screen.getByText(text); //Comes from testing library - Search a element in rendered component before
 
-        fireEvent.click(btn);
+        fireEvent.click(btn); //Comes from testing library - executes a event, on this case, execute click action on searched element
 
-        expect(mockNavigate).toHaveBeenCalled();
-        expect(mockNavigate).toHaveBeenCalledWith(path);
+        expect(mockNavigate).toHaveBeenCalled();//Expect Comes from jest - Expected navigation function to be called
+        expect(mockNavigate).toHaveBeenCalledWith(path);// and expected function to be called passing path value from iteration as parameter
     })
 })
